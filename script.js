9accb10245d371830186c6e87003e4bc85b05b63
Marina <perezeginamv@mail.ru>
2022-01-23 02:28:40 +0500

    Add script
    Test

---
+++ script.js
@@ -0,0 +1,13 @@
+function functionName(){
+    alert("Hello")
+}
+function work(){
+    alert("New branch")
+}
+function sayBay(){
+    alert("Goodbye from Git!")
+}
+
+function less() {
+    alert("Hello!")
+}
